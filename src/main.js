d77d412e826be1b012a6114030da3b1bb3aa41b4
dangtin306 <dangtin306@gmail.com>
2022-09-17 15:52:52 +0700

    okluonmm

--- src/main.js
+++ src/main.js
@@ -22,7 +22,8 @@ import '@ionic/vue/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-
+import './css/bootstrap.css';
+import './css/bootstrap.min.css';
 const app = createApp(App)
   .use(IonicVue)
   .use(router);
